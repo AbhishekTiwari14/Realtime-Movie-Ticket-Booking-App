1b5d9bf091fe914d82bd1d7ade91862887d25953
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-03-19 16:44:13 +0530

    minor changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import { MoviesList } from "@/components/MoviesList"
-import { dailyTaskUpdates } from "@/lib/generateMovieData"
 
 export function Homepage() {
-  dailyTaskUpdates()
   return <MoviesList />
 }
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,7 +1,9 @@
 import { MovieDetails } from "@/components/MovieDetails"
 import { MoviePoster } from "@/components/MoviePoster"
+import { dailyTaskUpdates } from "@/lib/generateMovieData"
 
 export function Moviepage() {
+  dailyTaskUpdates()
   return (
     <>
       <MoviePoster />
