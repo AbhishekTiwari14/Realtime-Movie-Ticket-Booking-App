98958b18169761da356efe876dc533ef48358636
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-03-27 18:09:30 +0530

    corrected daily tasks implementation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,10 @@
 import { MoviesList } from "@/components/MoviesList"
+import { dailyTaskUpdates } from "@/lib/generateMovieData"
+import { useEffect } from "react"
 
 export function Homepage() {
+  useEffect(() => {
+    dailyTaskUpdates()
+  }, [])
   return <MoviesList />
 }
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,9 +1,8 @@
 import { MovieDetails } from "@/components/MovieDetails"
 import { MoviePoster } from "@/components/MoviePoster"
-import { dailyTaskUpdates } from "@/lib/generateMovieData"
 
 export function Moviepage() {
-  dailyTaskUpdates()
+  
   return (
     <>
       <MoviePoster />
