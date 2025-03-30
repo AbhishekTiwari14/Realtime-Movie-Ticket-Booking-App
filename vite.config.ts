e6a8113969ef0bf1f57e4cf0522ec8e4589eb0c4
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-03-30 15:03:17 +0530

    css code split

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    cssCodeSplit: true,
+    // Extract critical CSS
+    cssMinify: true,
+  },
 })
